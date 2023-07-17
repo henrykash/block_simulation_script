2f4f909b964b9ce789885fd92e53e25e418466dc
henrykash <henrykariuki003@gmail.com>
2023-07-17 14:24:24 +0300

    implemented decoding transaction data from the transaction hash

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,5 +5,6 @@ if (!process.env.JSON_RPC_URL || !process.env.WSS_URL) {
 }
 export const config = {
     WSS_URL: process.env.WSS_URL!,
-    JSON_RPC_URL: process.env.JSON_RPC_URL!
+    JSON_RPC_URL: process.env.JSON_RPC_URL!,
+    PANCAKESWAP_ROUTER: "0x10ED43C718714eb63d5aA57B78B54704E256024E"
 }
--- src/core/blocksimulationwebsocket.ts
+++ src/core/blocksimulationwebsocket.ts
@@ -1,5 +1,9 @@
-import { ethers } from "ethers";
+import { constants, ethers, providers, utils } from "ethers";
 import { config } from "../config/config";
+import { PANCAKESWAP_ABI } from "../constants/abi";
+
+//create an interface of the pancake swap router abi
+let _pancakeSwap = new ethers.utils.Interface(PANCAKESWAP_ABI);
 
 export const streaming = async () => {
   const _wss_provider = new ethers.providers.WebSocketProvider(config.WSS_URL);
@@ -25,11 +29,11 @@ export const streaming = async () => {
         //extract the transaction hash, from , gas price, gas limit
         const { hash, from, gasPrice, gasLimit, blockNumber } = blockTransactions[i];
 
-        console.log("Transaction hash: ", hash)
-        console.log("Transaction from: ", from)
-        console.log("Transaction gas price: ", gasPrice)
-        console.log("Transaction gas limit: ", gasLimit)
-        console.log("Transaction block number: ", blockNumber)
+        // console.log("Transaction hash: ", hash)
+        // console.log("Transaction from: ", from)
+        // console.log("Transaction gas price: ", gasPrice)
+        // console.log("Transaction gas limit: ", gasLimit)
+        // console.log("Transaction block number: ", blockNumber)
 
         //push all the transaction in the block to the transactions array
         transactions.push({
@@ -43,16 +47,76 @@ export const streaming = async () => {
          //get the transaction receipt ftom the transaction hash
          const receipt = await _wss_provider.getTransaction(hash);
 
-         console.log("Transaction receipt: ", receipt)
+         //decode the transaction data: call the decodeData function
+         receipt?.hash &&  await decodeData(receipt);
+
          
        }
 
-       //sort the transactions in the block in descending order of gas price
+       //SORT the block transactions in the block in descending order of GAS PRICE
         const sortedTransactions = transactions.sort((a, b) => b.gasPrice - a.gasPrice);
-       // console.log("Here are the sorted transactions: ", sortedTransactions)
+        console.log("Here are the sorted transactions: ", sortedTransactions)
       
     });
   } catch (error) {
     console.log("Error in streaming: ", error);
   }
 };
+
+export const decodeData = async(receipt:   providers.TransactionResponse) => {
+
+  //implemenation of transaction processing logic goes here
+  let {
+    value: targetAmountInWei,
+    gasPrice: targetGasPriceInWei,
+    gasLimit: targetGasLimit,
+    to: router,
+    from: targetFrom,
+    hash: targetHash,
+  } = receipt;
+
+   //check if transaction is from a specific router i.e pancakeswap router
+   if (router?.toLowerCase() === config.PANCAKESWAP_ROUTER.toLowerCase()) {
+
+    try {
+
+     //decode the transaction data
+      let tx = _pancakeSwap.parseTransaction({
+        data: receipt.data,
+      });
+
+      let { name: targetMethodName, args: targetArgs } = tx;
+
+      let targetGasFeeInBNB = utils.formatEther(
+        targetGasLimit.mul(targetGasPriceInWei || constants.Zero)
+      );
+
+
+      let { path, amountOutMin: targetAmountOutMin } = targetArgs;
+
+      //if the path is undefined stop execution and return
+      if (!path) return;
+
+      let targetFromToken = path[0];
+      let targetToToken = path[path.length - 1];
+     
+      console.info({
+        targetFromToken,
+        targetToToken,
+        targetAmountInWei,
+        targetGasPriceInWei,
+        targetGasLimit,
+        targetGasFeeInBNB,
+        targetAmountOutMin,
+        targetMethodName,
+        targetFrom,
+        targetHash,
+      })
+    
+    } catch (error) {
+      console.log("Error in decodeData: ", error)
+    }
+      
+   }
+  
+}
