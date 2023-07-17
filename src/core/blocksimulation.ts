3760b5028c0eb0729d22b4dd2df5850a02157dc9
henrykash <henrykariuki003@gmail.com>
2023-07-17 12:35:50 +0300

    sorted all transaction in a block based on gasPrice in descendinf order

--- src/core/blocksimulation.ts
+++ src/core/blocksimulation.ts
@@ -10,52 +10,14 @@ export const blockSimulation = async () => {
     const blockNumber = await _provider.getBlockNumber();
 
     // Get the latest block
-    const block = await _provider.getBlock(blockNumber);
+    const block = await _provider.getBlockWithTransactions(blockNumber);
 
-    if (block) {
-
-      //if (txReceipt) {
-      // console.log("transaction reciept: ", txReceipt);
-      // Create an empty array to store transactions
-      const transactions = [];
-      for (const txHash of block.transactions) {
-        console.log("txHash: ", txHash)
-        //Get the transaction  transaction details
-        const txReceipt: any = await _provider.getTransaction(txHash);
-        // Simulate the transaction execution
-        const simulatedTx: any = await _provider.call(txReceipt);
-
-        console.log("We are here now", simulatedTx);
-
-        if(simulatedTx === "0x"){
-            console.log("Transaction failed")
-            return
-        }
-
-        console.log("We are here now 2")
-
-        // Calculate the gas fee
-        const gasPrice = ethers.utils.formatUnits(txReceipt.gasPrice, "gwei");
-
-        console.log("gas price: ", gasPrice)
-        const gasUsed = ethers.BigNumber.from(simulatedTx.gasUsed);
-        console.log("gas used: ", gasUsed)
-        const gasFee = ethers.utils.formatEther(
-          BigNumber.from(gasPrice).mul(gasUsed)
-        );
-
-        console.log("gas fee: ", gasFee)
-
-        // Push the transaction details with the gas fee to the transactions array
-        transactions.push({
-          hash: txReceipt.hash,
-          //   gasFee: parseFloat(gasFee),
-        });
-      }
-
-      console.log("Transactions: ", transactions);
-      //}
+    if (!block) {
+      return;
     }
+
+
+  
   } catch (error) {
     console.log("Error in blockSimulation: ", error);
   }
---
+++ src/core/blocksimulationwebsocket.ts
@@ -0,0 +1,57 @@
+import { ethers } from "ethers";
+import { config } from "../config/config";
+
+export const streaming = async () => {
+  const _wss_provider = new ethers.providers.WebSocketProvider(config.WSS_URL);
+
+  try {
+    // Subscribe to the newBlockHeaders event
+    _wss_provider.on("block", async (blockNumber) => {
+      console.log("New block: ", blockNumber);
+
+      // Get the transactions in the block
+      const block: any = await _wss_provider.getBlockWithTransactions(blockNumber);
+
+      if (!block) {
+        return;
+      }
+      // Create an empty array to store transactions
+      const transactions = [];
+
+      let blockTransactions = block.transactions;
+      //loop through the transactions in the block and get the transaction details
+       for (let i = 0; i < blockTransactions.length; i++) {
+        //extract the transaction hash, from , gas price, gas limit
+        const { hash, from, gasPrice, gasLimit, blockNumber } = blockTransactions[i];
+
+        // console.log("Transaction hash: ", hash)
+        // console.log("Transaction from: ", from)
+        // console.log("Transaction gas price: ", gasPrice)
+        // console.log("Transaction gas limit: ", gasLimit)
+        // console.log("Transaction block number: ", blockNumber)
+
+        //push all the transaction in the block to the transactions array
+        transactions.push({
+          hash: hash,
+          from: from,
+          gasPrice: gasPrice,
+          gasLimit: gasLimit,
+          blockNumber: blockNumber
+        });
+ 
+         //get the transaction receipt ftom the transaction hash
+         const receipt = await _wss_provider.getTransaction(hash);
+
+         console.log("Transaction receipt: ", receipt)
+         
+       }
+
+       //sort the transactions in the block in descending order of gas price
+        const sortedTransactions = transactions.sort((a, b) => b.gasPrice - a.gasPrice);
+        console.log("Here are the sorted transactions: ", sortedTransactions)
+      
+    });
+  } catch (error) {
+    console.log("Error in streaming: ", error);
+  }
+};
